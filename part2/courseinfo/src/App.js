511b389c6a95881489f5f6fff269cd5719c9c737
Jamshed Siddiqi <jsiddiqi@cisco.com>
2020-12-27 13:22:54 +0200

    Ex-2 refactor

--- part2/courseinfo/src/App.js
+++ part2/courseinfo/src/App.js
@@ -2,102 +2,21 @@ import React from "react"
 import Note from './components/Note'
 
 
-const Header = ({ courseName }) => (
-  <h1>
-    {courseName}
-  </h1>
-)
-
-const Total = ({ parts }) => {
-  const exList = parts.map(part => part.exercises)
-  const sum = exList.reduce( (total, curValue) => { return total + curValue} )
-  return(
-  <h3>
-    total of {sum} exercises
-  </h3>)
-
-}
-
-const Course = ({ course }) => (
-  <div>
-    <Header courseName={course.name} />
-    <Note parts = {course.parts}/>
-    <Total parts = {course.parts}/>
-  </div>
-)
-
-const App = () => {
-  const courses = [
-    {
-      name: 'Half Stack application development',
-      id: 1,
-      parts: [
-        {
-          name: 'Fundamentals of React',
-          exercises: 10,
-          id: 1
-        },
-        {
-          name: 'Using props to pass data',
-          exercises: 7,
-          id: 2
-        },
-        {
-          name: 'State of a component',
-          exercises: 14,
-          id: 3
-        },
-        {
-          name: 'Redux',
-          exercises: 11,
-          id: 4
-        }
-      ]
-    }, 
-    {
-      name: 'MongoDb',
-      id: 2,
-      parts: [
-        {
-          name: 'Db basic',
-          exercises: 5,
-          id: 1
-        },
-        {
-          name: 'Db connection',
-          exercises: 7,
-          id: 2
-        }
-      ]
-    }, 
-    {
-      name: 'Node.js',
-      id: 2,
-      parts: [
-        {
-          name: 'Routing',
-          exercises: 3,
-          id: 1
-        },
-        {
-          name: 'Middlewares',
-          exercises: 7,
-          id: 2
-        }
-      ]
-    }
-  ]
-
-  const courseList = courses.map( c => {
+const NoteList = ({notes}) =>{
+  const noteList = notes.map( note => {
     return(
     <div>
-      <Course course={c}/>
+      <Note note={note}/>
     </div>)
   })
 
-  return courseList
-
-  //return <Course course={courses} />
+  return noteList
 }
+
+const App = ({notes}) => (
+  <div>
+    <NoteList notes ={notes}/>
+  </div>
+)
 
 export default App
--- part2/courseinfo/src/components/Note.js
+++ part2/courseinfo/src/components/Note.js
@@ -1,9 +1,33 @@
 import React from "react"
 
 
-const Note = ({ parts }) => {
+const Titles = ({ parts }) => {
 const titles = parts.map( part => <p>{part.name} {part.exercises}</p>)
     return titles
 }
+
+const Header = ({ courseName }) => (
+    <h1>
+      {courseName}
+    </h1>
+  )
+  
+  const Total = ({ parts }) => {
+    const exList = parts.map(part => part.exercises)
+    const sum = exList.reduce( (total, curValue) => { return total + curValue} )
+    return(
+    <h3>
+      total of {sum} exercises
+    </h3>)
+  
+  }
+  
+  const Note = ({ note }) => (
+    <div>
+      <Header courseName={note.name} />
+      <Titles parts = {note.parts}/>
+      <Total parts = {note.parts}/>
+    </div>
+  )
 
 export default Note
